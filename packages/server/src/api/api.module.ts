4aeab600229820ce16ef48af6541544e5d20f087
Eugene Pashkovskiy <pashkovskiy.eugen@gmail.com>
2023-12-02 14:53:41 +0200

    add more modules

--- packages/server/src/api/api.module.ts
+++ packages/server/src/api/api.module.ts
@@ -5,9 +5,20 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { InvoicesModule } from './invoices/invoices.module';
 import { PlacesModule } from './places/places.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
+import { EventsModule } from './events/events.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, InvoicesModule, PlacesModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    InvoicesModule,
+    PlacesModule,
+    VehiclesModule,
+    EventsModule,
+    EmailModule,
+  ],
   controllers: [ApiController],
   providers: [ApiService],
 })
--- packages/server/src/api/places/places.service.ts
+++ packages/server/src/api/places/places.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Place } from './entities/place.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PlacesService {}
+export class PlacesService {
+  constructor(
+    @InjectRepository(Place) private placeRepository: Repository<Place>,
+  ) {}
+
+  public getPlace(id: string) {
+    return this.placeRepository.findOneBy({ id });
+  }
+}
